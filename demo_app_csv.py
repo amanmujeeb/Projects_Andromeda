522e8b0844ca38bf2651148480413e5ecb4e418b
Aman <87927917+amanmujeeb@users.noreply.github.com>
2023-06-25 12:48:51 +0530

    Add files via upload

--- demo_app_csv.py
+++ demo_app_csv.py
@@ -13,6 +13,7 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.chains import RetrievalQA
+import pandas as pd
 
 def generate_response(uploaded_file, openai_api_key, query_text):
     # Load document if file is uploaded
